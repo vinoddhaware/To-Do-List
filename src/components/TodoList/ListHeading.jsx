fca8deba044aeecec4152ea6c3fa7bebd2738c72
vinoddhaware <vinoddhaware>
2024-10-19 16:18:12 +0530

    remove: some padding

--- src/components/TodoList/ListHeading.jsx
+++ src/components/TodoList/ListHeading.jsx
@@ -4,25 +4,25 @@ const ListHeading = () => {
   return (
       <thead className="border-b-2 text-sm sticky top-0 backdrop-blur-3xl shadow-md rounded-md">
         <tr className="">
-          <th scope="col" className="px-6 py-3 break-all">
+          <th scope="col" className="px-6 py-3">
             <input type="checkbox" />
           </th>
-          <th scope="col" className="px-6 py-3 break-all">
+          <th scope="col" className="px-6 py-3">
             Assigned To
           </th>
-          <th scope="col" className="px-6 py-3 break-all">
+          <th scope="col" className="px-6 py-3">
             Status
           </th>
-          <th scope="col" className="px-6 py-3 break-all">
+          <th scope="col" className="px-6 py-3">
             Due Date
           </th>
-          <th scope="col" className="px-6 py-3 break-all">
+          <th scope="col" className="px-6 py-3">
             Priority
           </th>
-          <th scope="col" className="px-6 py-3 break-all">
+          <th scope="col" className="px-6 py-3">
             Comments
           </th>
-          <th scope="col" className="px- py-3 break-all">
+          <th scope="col" className="px- py-3">
             Action
           </th>
         </tr>
